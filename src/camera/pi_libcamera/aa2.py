3480820dc5bb1e870acec253995ce35bc9b8d6ea
jiwoo <jju0531@naver.com>
2025-04-11 20:18:43 +0900

    스티칭실험4

--- src/camera/pi_libcamera/aa2.py
+++ src/camera/pi_libcamera/aa2.py
@@ -4,6 +4,7 @@ import subprocess
 import shlex
 import threading
 
+
 def start_camera(camera_id, frame_holder):
     cmd = f"libcamera-vid --inline --nopreview -t 0 --codec mjpeg --width 400 --height 240 --framerate 30 -o - --camera {camera_id}"
     process = subprocess.Popen(
@@ -30,6 +31,7 @@ def start_camera(camera_id, frame_holder):
 
     process.terminate()
 
+
 # 프레임 저장
 frameL = [None]
 frameR = [None]
@@ -42,7 +44,7 @@ threading.Thread(target=start_camera, args=(1, frameR)).start()
 orb = cv2.ORB_create(1000)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 homography = None
-update_every = 30  # 몇 프레임마다 H 계산
+update_every = 5  # 몇 프레임마다 H 계산
 frame_count = 0
 
 try:
@@ -62,10 +64,14 @@ try:
             if des1 is not None and des2 is not None:
                 matches = bf.match(des1, des2)
                 matches = sorted(matches, key=lambda x: x.distance)
-                
+
                 if len(matches) > 10:
-                    pts1 = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-                    pts2 = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+                    pts1 = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(
+                        -1, 1, 2
+                    )
+                    pts2 = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(
+                        -1, 1, 2
+                    )
 
                     # Homography 계산
                     H, _ = cv2.findHomography(pts2, pts1, cv2.RANSAC, 5.0)
@@ -74,8 +80,10 @@ try:
 
         if homography is not None:
             # 스티칭
-            warped = cv2.warpPerspective(right, homography, (left.shape[1]*2, left.shape[0]))
-            warped[0:left.shape[0], 0:left.shape[1]] = left
+            warped = cv2.warpPerspective(
+                right, homography, (left.shape[1] * 2, left.shape[0])
+            )
+            warped[0 : left.shape[0], 0 : left.shape[1]] = left
 
             cv2.imshow("Live Panorama", warped)
 
